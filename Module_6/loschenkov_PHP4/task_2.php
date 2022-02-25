0396edfbb30b8617db6dd33f0e0bf04795a86846
InventiveSpark <ReadingDude@gmail.com>
2022-02-25 20:38:34 +0500

    PHP4

---
+++ Module_6/loschenkov_PHP4/task_2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Самостоятельная работа &#8470;4.2. Массивы. Строки. Циклы</title>
+</head>
+
+<body>
+    <?php
+    $i = 0;
+    while ($i++ <= 10) {
+        switch ($i) {
+            case 5:
+                echo "Итерация 5 ";
+                break;
+            case 10:
+                echo "Итерация 10";
+                break;
+        }
+    }
+    // Итерация 5 Итерация 10
+    ?>
+</body>
+
+</html>
